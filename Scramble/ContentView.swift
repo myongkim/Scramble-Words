739f1de96d7b09697c4020aa74a0dadc56559b05
Isaac Kim <myong328@gmail.com>
2022-04-08 09:12:39 -0700

    Initial Commit

---
+++ Scramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scramble
+//
+//  Created by Isaac Kim on 4/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scramble/ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambleApp.swift
+//  Scramble
+//
+//  Created by Isaac Kim on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
